2ab39050e58cac5e260b8e8d4c4931e678566ecd
wangchangfu <2690022182@qq.com>
2019-01-17 11:10:31 +0800

    redis配置更新 之前不需要密码

--- project-admin/src/main/java/com/mapscience/config/SwaggerConfig.java
+++ project-admin/src/main/java/com/mapscience/config/SwaggerConfig.java
@@ -18,7 +18,6 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                         //这里采用包含注解的方式来确定要显示的接口
                 .apis(RequestHandlerSelectors.basePackage("com.mapscience.modular.system.controller"))    //这里采用包扫描的方式来确定要显示的接口
                 .paths(PathSelectors.any())
                 .build();
