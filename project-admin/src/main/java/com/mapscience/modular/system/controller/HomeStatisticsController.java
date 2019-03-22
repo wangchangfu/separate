26e6d055fd4a1fd60254ed33a7f534904ca2b108
zhoux <842410157@qq.com>
2019-03-22 12:31:18 +0800

    更新

--- project-admin/src/main/java/com/mapscience/modular/system/controller/HomeStatisticsController.java
+++ project-admin/src/main/java/com/mapscience/modular/system/controller/HomeStatisticsController.java
@@ -78,10 +78,10 @@ public class HomeStatisticsController extends BaseController {
         return  comList;
     }
 
-    @ApiOperation(value = "通过公司id查询员工合同分布")
+    @ApiOperation(value = "在职员工合同分布")
     @RequestMapping(value = "findContractByCompanyId",method = RequestMethod.POST)
     @ResponseBody
-    public ResponseVal findContractByCompanyId(String companyId, 
+    public ResponseVal findContractByCompanyId(@RequestParam(required=false)String companyId, 
     		@RequestParam(required=false, defaultValue="固定期,无固定,实习协议,劳务合同,其他") String contractType,
     		@RequestParam(required=false, defaultValue="3") int numberOfYearAgo){
     	try {
@@ -93,10 +93,10 @@ public class HomeStatisticsController extends BaseController {
 		}
     }
 
-    @ApiOperation(value = "通过公司id查询员工学历分布")
+    @ApiOperation(value = "在职员工学历分布")
     @RequestMapping(value = "findEducationByCompanyId",method = RequestMethod.POST)
     @ResponseBody
-    public ResponseVal findEducationByCompanyId(String companyId){
+    public ResponseVal findEducationByCompanyId(@RequestParam(required=false)String companyId){
     	try {
     		HashMap<String, String> findEducationByCompanyId = educationService.findEducationByCompanyId(companyId);
     		return new ResponseVal(200,"success", findEducationByCompanyId);
@@ -109,9 +109,9 @@ public class HomeStatisticsController extends BaseController {
     @ApiOperation(value = "员工年龄段分布图")
     @RequestMapping(value = "employeeAgeDistributionMap",method = RequestMethod.POST)
     @ResponseBody
-    public ResponseVal employeeAgeDistributionMap(String companyId,
-    		@RequestParam(required=false, defaultValue="0-25,26-30,31-35,36-40,41-45,46-50,51-200") String ageRange,
-    		@RequestParam(required=false, defaultValue="男,女") String gender){
+    public ResponseVal employeeAgeDistributionMap(@RequestParam(required=false)String companyId,
+    		@RequestParam(required=false, defaultValue="0-25,26-30,31-35,36-40,41-45,46-50,51-200")String ageRange,
+    		@RequestParam(required=false, defaultValue="男,女")String gender){
     	try {
     		ArrayList<HashMap<String, String>> employeeAgeDistributionMap = employeeService.employeeAgeDistributionMap(companyId, ageRange, gender);
     		return new ResponseVal(200,"success",employeeAgeDistributionMap);
